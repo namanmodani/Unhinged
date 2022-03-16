fa3b7f291d4fc9e9df7eef5645e28dbfd50fe334
Naman Modani <modani.naman@gmail.com>
2022-03-16 06:51:42 -0700

    Update utility.h

--- utility.h
+++ utility.h
@@ -7,10 +7,15 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include <functional>
+#include "provided.h"
 
-class utility {
-
+class Hash {
+public:
+    unsigned int operator()(const AttValPair& av) const;
+    unsigned int operator()(const EmailCount& ec) const;
 };
 
+bool comp(const EmailCount& ec1, const EmailCount& ec2);
 
 #endif //UTILITY_H
