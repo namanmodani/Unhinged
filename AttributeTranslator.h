c3fe3ed825b8584505c8d0e77d72fe4d0dc16086
Naman Modani <modani.naman@gmail.com>
2022-03-15 22:49:35 -0700

    AttributeTranslator .h Setup

--- AttributeTranslator.h
+++ AttributeTranslator.h
@@ -7,10 +7,22 @@
 #ifndef ATTRIBUTETRANSLATOR
 #define ATTRIBUTETRANSLATOR
 
+#include <vector>
+#include <set>
+#include <string>
+#include "provided.h"
+#include "RadixTree.h"
 
-class AttributeTranslator {
-
+class AttributeTranslator
+{
+public:
+    AttributeTranslator();
+    ~AttributeTranslator();
+    bool Load(std::string filename);
+    std::vector<AttValPair> findCompatible(const AttValPair& source) const;
+private:
+    RadixTree<std::vector<AttValPair>*>* m_RadixTreePairToPair;
+    std::set<std::string>* m_SourceAttValSet;
 };
-
 
 #endif //ATTRIBUTETRANSLATOR
