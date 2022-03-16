d0c9084dfe6346d15e1aff924f58f4da3c1e1166
Naman Modani <modani.naman@gmail.com>
2022-03-16 06:02:56 -0700

    Fixed public and private members of PersonProfile

--- PersonProfile.h
+++ PersonProfile.h
@@ -4,14 +4,32 @@
 // PersonProfile.h
 //
 
-
 #ifndef PERSONPROFILE_H
 #define PERSONPROFILE_H
 
+#include <string>
+#include "RadixTree.h"
+#include <vector>
+#include <set>
 
-class PersonProfile {
+struct AttValPair;
 
+class PersonProfile
+{
+public:
+    PersonProfile(std::string name, std::string email);
+    ~PersonProfile();
+    std::string GetName() const;
+    std::string GetEmail() const;
+    void AddAttValPair(const AttValPair& attval);
+    int GetNumAttValPairs() const;
+    bool GetAttVal(int attribute_num, AttValPair& attval) const;
+private:
+    std::string m_name;
+    std::string m_email;
+    int m_countAttValPairs;
+    RadixTree<std::set<std::string>*>* m_rtreeAttVal;
+    std::vector<AttValPair*>* m_attvalVec;
 };
-
 
 #endif //PERSONPROFILE_H
